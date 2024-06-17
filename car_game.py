3e6e8792161ec97d9e5f80d1b6db9c6fa29a60cb
farhanabdulkhaliq <farhanabdulkhaliq2009@gmail.com>
2024-06-17 10:10:30 +0530

    car_game.py

--- car_game.py
+++ car_game.py
@@ -1,5 +1,15 @@
+import pygame
+
 command = ''
 started = False
+
+pygame.init()
+pygame.mixer.init()
+
+bye_sfx = pygame.mixer.Sound('Car_game/bye.wav')
+start_sfx = pygame.mixer.Sound('Car_game/car_start.mp3')
+engine_sfx = pygame.mixer.Sound('Car_game/car_rev.mp3')
+car_stop_sfx = pygame.mixer.Sound('Car_game/car_stop.wav')
 
 while True:
     command = input('> ').lower()
@@ -7,19 +17,39 @@ while True:
         print('start - to start type start')
         print('stop - to stop type stop')
         print('quit - to exit type quit')
+        print('rev engine - to rev engine type rev engine')
     elif command == 'start':
         if started:
             print('Car is already started')
         else:
             started = True
-            print('Car started... Ready to go')
+            start_sfx.play()
+            print('Car started... Ready to go')             
     elif command == 'stop':
         if not started  :
             print('Car is already stopped')
         else:
             started = False
             print('Car stopped')
+            engine_sfx.stop()
+            start_sfx.stop()
+            car_stop_sfx.play()
+    elif command == 'rev engine' and not started:
+        print('Car is not started!')
+    elif command == 'rev engine' and started:
+        print('Vroom! Vroom!')
+        engine_sfx.play()
+       
     elif command == 'quit':
+        engine_sfx.stop()
+        car_stop_sfx.stop()
+        engine_sfx.stop()
+        bye_sfx.play()
         break
+    
     else :
         print("Sorry, I don't understand that")
+
+pygame.mixer.quit()
+pygame.quit()
+
